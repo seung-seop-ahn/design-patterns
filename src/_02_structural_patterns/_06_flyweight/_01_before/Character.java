765f3b6d6b2a04e0a8c7ae8b2f45ddeb93ac7b21
Kevin Ahn <seungseopahn@gmail.com>
2024-09-20 13:37:30 +1200

    feat: flyweight

---
+++ src/_02_structural_patterns/_06_flyweight/_01_before/Character.java
@@ -0,0 +1,19 @@
+package _02_structural_patterns._06_flyweight._01_before;
+
+public class Character {
+
+    private char value;
+
+    private String color;
+
+    private String fontFamily;
+
+    private int fontSize;
+
+    public Character(char value, String color, String fontFamily, int fontSize) {
+        this.value = value;
+        this.color = color;
+        this.fontFamily = fontFamily;
+        this.fontSize = fontSize;
+    }
+}
